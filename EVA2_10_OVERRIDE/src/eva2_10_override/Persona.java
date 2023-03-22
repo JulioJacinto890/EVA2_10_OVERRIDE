80645aca4af18a01b0a0e9111c63b43e4ac4b5bf
JulioJacinto890 <124766374+JulioJacinto890@users.noreply.github.com>
2023-03-21 18:57:05 -0600

    Add files via upload

---
+++ EVA2_10_OVERRIDE/src/eva2_10_override/Persona.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_10_override;
+
+/**
+ *
+ * @author hp
+ */
+public class Persona {
+
+    private String nombre;
+    private int edad;
+    
+     Persona(){
+        this.nombre = "___________";
+        this.edad = 0;
+    }
+     
+     Persona(String nombre,int edad){
+         this.nombre = nombre;
+         this.edad = edad;
+         
+     }
+     
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    @Override
+    public String toString(){
+        //no estamos invocando al metdo sobreescrito de la clase, lo reemplazamos de la superclase
+        String cade="Datos:\n"
+                +"Nombre: "+nombre+"\n"+
+                "Edad: "+edad;
+        
+        return cade;
+    }
+}
+     
+
+    
+
